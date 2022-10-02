ff54d1afd190f6523f74c8870081fe816c8b28d5
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-10-02 22:00:32 +0900

    Initial Commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Company Name';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Blogs';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '©2022 Company Name. All rights reserved.';
 
   return {
     name,
